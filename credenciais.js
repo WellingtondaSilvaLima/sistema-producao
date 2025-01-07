3159c60e3a4d8137003d0f7ea152b49220d919a5
Wellington Lima <wellingtonlima.jt@gmail.com>
2025-01-07 14:42:04 -0300

    Credenciais

---
+++ credenciais.js
@@ -0,0 +1,10 @@
+const credenciais = {
+  user: 'simplo',
+  senha: 'Simplo@21051',
+  id_url_caminhoes: "2PACX-1vRL8Io571pF71ozUOjExaQHcMpkz-Ps1uwJzLV34hnzSlc0sU8WnRS2hEn3VZ4-5faiVcXT1y3TVFz3",
+  id_url_carros: "2PACX-1vQmLz6-lnHC_-9RB6L2GEWDsmD0eTwGw9jCmrwlyLLfzLPC7SJA9vy8AngEZRmuIOShLZJ4moNvLi5c",
+  id_url_hib_ele: "2PACX-1vQj0N4ThwivLk5wKuu4n0fhfJK3nq7JA9EjSXU5nDornDfiKekESZFN94zxAeCfVNibQreEVYcLhGsS",
+  id_url_motos: "2PACX-1vTiyh-S9W80RFrPR6BuxXo2_EgJUk1aUko7rB4P9SlMcsSi5Lg3bm4yLDncQ4rsv57879OMCKpnV--w"
+}
+
+export default credenciais;
